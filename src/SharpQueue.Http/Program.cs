08f4a36980ed9411170fdce427e867e4ca54d2d6
mattheuspirovani <mattheus.pirovani@gmail.com>
2024-10-06 07:01:36 -0300

    feat: Add HTTP interface for SharpQueue
    - Implemented REST API for producers and consumers to interact with the message queue.
    - Added QueueController with endpoints to add and consume messages via HTTP..
    - Configured routing and middleware for HTTP communication.
    - Enabled Swagger for API documentation and testing.

---
+++ src/SharpQueue.Http/Program.cs
@@ -0,0 +1,21 @@
+using SharpQueue;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSingleton<QueueManager>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
